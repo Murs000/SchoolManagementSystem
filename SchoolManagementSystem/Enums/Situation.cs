ecf3347fde3e3563d59b863ebd3b0d786e139610
Murs000 <vizortele096@gmail.com>
2023-04-20 18:03:00 +0400

    App. Add situation emum and crud operation buttons

---
+++ SchoolManagementSystem/Enums/Situation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementSystem.Enums
+{
+    public enum Situation
+    {
+        Default = 0,
+        Add = 1,
+        Edit = 2,
+        Selected = 3
+    }
+}
