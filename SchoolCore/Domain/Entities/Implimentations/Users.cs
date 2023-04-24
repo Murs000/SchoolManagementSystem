9b79da64c59b63d7f9f628d5488ef71261236ddd
Murs000 <vizortele096@gmail.com>
2023-04-24 18:45:26 +0400

    Core. Add user entity and entity interface

---
+++ SchoolCore/Domain/Entities/Implimentations/Users.cs
@@ -0,0 +1,25 @@
+﻿using SchoolCore.Domain.Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolCore.Domain.Entities.Implimentations
+{
+    internal class User : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int? CreatorId => Creator?.Id;
+        public int? ModifierId => Modifier?.Id;
+
+        public User Creator { get; set; }
+        public User Modifier { get; set; }
+    }
+}
---
+++ SchoolCore/Domain/Entities/Interfaces/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolCore.Domain.Entities.Interfaces
+{
+    internal interface IEntity
+    {
+        int Id { get; set; }
+    }
+}
