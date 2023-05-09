33d5faebe8bc4eaed21157147e081eef2cb3057a
Murs000 <vizortele096@gmail.com>
2023-05-09 15:24:55 +0400

    App. Add class services

---
+++ SchoolManagementSystem/Services/Implimentations/ClassService.cs
@@ -0,0 +1,89 @@
+﻿using ClosedXML.Excel;
+using Microsoft.Win32;
+using SchoolCore.DataAccess.Interfaces;
+using SchoolCore.Domain.Entities.Implimentations;
+using SchoolManagementSystem.Mappers.Interfaces;
+using SchoolManagementSystem.Models;
+using SchoolManagementSystem.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementSystem.Services.Implimentations
+{
+    internal class ClassService : IClassService
+    {
+        private readonly IUnitOfWork _db;
+        private readonly IClassMapper _classMapper;
+        public ClassService(IUnitOfWork db, IClassMapper classMapper)
+        {
+            _db = db;
+            _classMapper = classMapper;
+        }
+        public List<ClassModel> GetAll()
+        {
+            List<ClassModel> classModels = new List<ClassModel>();
+            List<Class> classes = _db.ClassRepository.GetAll();
+
+            int no = 1;
+
+            foreach (var clas in classes)
+            {
+                ClassModel classModel = _classMapper.Map(clas);
+
+                classModel.No = no++;
+
+                classModels.Add(classModel);
+            }
+
+            return classModels;
+        }
+
+        public int Save(ClassModel classModel)
+        {
+            Class willSavedClass = _classMapper.Map(classModel);
+
+            willSavedClass.Modifier = new User { Id = 4 };
+            willSavedClass.ModifiedDate = DateTime.Now;
+
+            if (willSavedClass.Id == 0)
+            {
+                willSavedClass.CreationDate = DateTime.Now;
+                willSavedClass.Creator = new User() { Id = 4 };
+
+                return _db.ClassRepository.Insert(willSavedClass);
+            }
+            else
+            {
+                var existingAuthor = _db.TeacherRepository.GetById(classModel.Id);
+
+                willSavedClass.CreationDate = existingAuthor.CreationDate;
+                willSavedClass.Creator = existingAuthor.Creator;
+
+                _db.ClassRepository.Update(willSavedClass);
+
+                return willSavedClass.Id;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            Class clas = _db.ClassRepository.GetById(id);
+
+            clas.IsDeleted = true;
+            clas.ModifiedDate = DateTime.Now;
+            clas.Modifier = new User { Id = 4 };
+
+            return _db.ClassRepository.Update(clas);
+        }
+
+        public void Exel()
+        {
+            
+        }
+    }
+}
---
+++ SchoolManagementSystem/Services/Interfaces/IClassService.cs
@@ -0,0 +1,17 @@
+﻿using SchoolManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementSystem.Services.Interfaces
+{
+    internal interface IClassService
+    {
+        List<ClassModel> GetAll();
+        int Save(ClassModel classModel);
+        bool Delete(int id);
+        void Exel();
+    }
+}
